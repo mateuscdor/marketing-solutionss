63d7af78016f89a4312cc862cc212a5317b26761
Guilherme Cunha <guiscunha@gmail.com>
2022-08-08 10:49:22 -0300

    fix: destinations filter

--- url-proxy/src/pages/api/destinations/index.ts
+++ url-proxy/src/pages/api/destinations/index.ts
@@ -18,6 +18,7 @@ export default async function handler(
     case "GET":
       const destinations = await DestinationModel.find({
         owner: query.owner,
+        redirect: query.redirectId,
       })
         .populate("redirect")
         .lean();
