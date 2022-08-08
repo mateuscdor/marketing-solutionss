d2ea6324ca73a98eb5e39ab56c86ce5411b3082b
Guilherme Cunha <guiscunha@gmail.com>
2022-08-08 00:37:50 -0300

    feat: default value in destinations

--- url-proxy/src/modules/redirects/home.tsx
+++ url-proxy/src/modules/redirects/home.tsx
@@ -72,7 +72,9 @@ const RedirectsHome = () => {
         ]}
         data={(entitiesResponse?.results || []).map((entity) => ({
           ...entity,
-          destinations: entity.destinations.map(({ url }) => url).join(", "),
+          destinations: (entity.destinations || [])
+            .map(({ url }) => url)
+            .join(", "),
         }))}
         actions={[
           {
