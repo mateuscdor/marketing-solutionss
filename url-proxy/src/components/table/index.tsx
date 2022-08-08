4eeba399484968411d64aec75632ca785cfd88eb
Guilherme Cunha <guilherme.s.cunha@accenture.com>
2022-08-07 21:13:33 -0300

    fix: starting table width fix

--- url-proxy/src/components/table/index.tsx
+++ url-proxy/src/components/table/index.tsx
@@ -18,7 +18,7 @@ export type TableProps = {
 const Table = ({ columns, data, actions }: TableProps) => {
   return (
     <div className="-mx-4 mt-8 overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:-mx-6 md:mx-0 md:rounded-lg">
-      <table className="min-w-full divide-y divide-gray-300">
+      <table className="flex flex-col w-full table-fixed divide-y divide-gray-300">
         <thead className="bg-gray-50">
           <tr>
             {columns.map((column) => (
@@ -35,16 +35,23 @@ const Table = ({ columns, data, actions }: TableProps) => {
             </th>
           </tr>
         </thead>
-        <tbody className="divide-y divide-gray-200 bg-white px-4">
+        <tbody className="flex flex-col w-full divide-y divide-gray-200 px-4">
           {data.map((item, itemIndex) => {
             return (
-              <tr key={item.id}>
-                {columns.map((column) => (
-                  <td key={`column-${item.id}-${column.key}`}>
-                    <span>{item[column.key]}</span>
-                  </td>
-                ))}
-                <td className="flex flex-col py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6 justify-start items-start lg:space-x-2 lg:flex-row lg:justify-end">
+              <tr key={item.id} className="flex w-full">
+                <div className="flex flex-row w-full">
+                  {columns.map((column) => (
+                    <td
+                      key={`column-${item.id}-${column.key}`}
+                      className="w-auto"
+                    >
+                      <span className="flex truncate w-full max-w-xs">
+                        {item[column.key]}
+                      </span>
+                    </td>
+                  ))}
+                </div>
+                <td className="flex flex-col py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6 justify-start items-start lg:space-x-2 lg:flex-row lg:justify-end w-auto">
                   {actions.map((actionProps, index) => {
                     return (
                       <Action
--- url-proxy/src/modules/redirects/home.tsx
+++ url-proxy/src/modules/redirects/home.tsx
@@ -67,7 +67,9 @@ const RedirectsHome: NextPage = () => {
         ]}
         data={(entitiesResponse?.results || []).map((entity) => ({
           ...entity,
-          destinations: entity.destinations.map(({ url }) => url).join(", "),
+          destinations: (entity.destinations || [])
+            .map(({ url }) => url)
+            .join(", "),
         }))}
         actions={[
           {
