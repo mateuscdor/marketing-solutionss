623ae85e5b4ef917d55701e601b8bd9f54db1535
Guilherme Cunha <guiscunha@gmail.com>
2022-08-09 12:07:13 -0300

    feat: previning duplicate calls in go page

--- url-proxy/src/pages/go.tsx
+++ url-proxy/src/pages/go.tsx
@@ -1,13 +1,16 @@
 import { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import FullscreenLoading from "../components/loading/fullscreen";
 import { RedirectsService } from "../services/redirects";
 
 function GoPage({ origin, userIp }: any) {
   const router = useRouter();
+  const alreadyCalledRef = useRef(false);
 
   useEffect(() => {
+    if (alreadyCalledRef.current) return;
+    console.debug(`==> requesting destination ${origin} - ${userIp}`);
     new RedirectsService()
       .getDestination(origin, {
         userIp,
@@ -17,6 +20,8 @@ function GoPage({ origin, userIp }: any) {
           router.push(destination.url);
         }
       });
+
+    alreadyCalledRef.current = true;
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return <FullscreenLoading />;
