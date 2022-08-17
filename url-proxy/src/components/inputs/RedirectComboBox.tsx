4bf2c48862d9a8b6fa5f05d32e710b85b2da7ca2
Guilherme Cunha <guilherme.s.cunha@accenture.com>
2022-08-17 12:21:57 -0300

    fix: combobox z-index

--- url-proxy/src/components/inputs/RedirectComboBox.tsx
+++ url-proxy/src/components/inputs/RedirectComboBox.tsx
@@ -36,7 +36,7 @@ export default function ComboBox({
   return (
     <div className="">
       <Combobox value={selected} onChange={setSelected}>
-        <div className="relative mt-1">
+        <div className="relative mt-1 z-20">
           <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-gray-300 text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
             <Combobox.Input
               className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
