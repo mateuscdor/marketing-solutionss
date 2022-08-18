29a81e4a9746512880e442f421c0ad5bb239518f
Guilherme Cunha <guilherme.s.cunha@accenture.com>
2022-08-17 23:40:48 -0300

    feat: removing go back function in redirection groups

--- url-proxy/src/modules/redirect-groups/index.tsx
+++ url-proxy/src/modules/redirect-groups/index.tsx
@@ -80,27 +80,6 @@ const RedirectGroupsHome: NextPage<RedirectGroupsHomeProps> = ({}) => {
 
   return (
     <div className="flex flex-col w-full h-full">
-      <div className="flex w-full justify-start">
-        <div className="p-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 cursor-pointer"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth={2}
-            onClick={() => {
-              router.back();
-            }}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M7 16l-4-4m0 0l4-4m-4 4h18"
-            />
-          </svg>
-        </div>
-      </div>
       <ManageHeader
         onCreateClick={() => {
           console.debug("Creating entity");
--- url-proxy/src/pages/redirect-groups/index.tsx
+++ url-proxy/src/pages/redirect-groups/index.tsx
@@ -1,6 +1,5 @@
 import RedirectGroupsHome from "../../modules/redirect-groups";
 import DashboardLayout from "../../layouts/dashboard";
-import { GetServerSideProps } from "next";
 
 const Page = (props: any) => (
   <DashboardLayout>
