312ecebaa1bf6cb462224ed180b3c9486a9c8c6e
Guilherme Cunha <guilherme.s.cunha@accenture.com>
2022-08-17 21:38:11 -0300

    fix: topbar z-index

--- url-proxy/src/layouts/dashboard.tsx
+++ url-proxy/src/layouts/dashboard.tsx
@@ -207,7 +207,7 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
           </div>
         </div>
         <div className="md:pl-64 flex flex-col flex-1">
-          <div className="sticky top-0 z-10 flex-shrink-0 flex h-16 bg-white shadow">
+          <div className="sticky top-0 z-40 flex-shrink-0 flex h-16 bg-white shadow">
             <button
               type="button"
               className="px-4 border-r border-gray-200 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden"
